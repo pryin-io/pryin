44966124d6aeec62cbb39116093330c17425c8ae
Manuel Kallenbach <mk@manukall.de>
2017-05-19 07:29:06 +0200

    release 1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PryIn.Mixfile do
 
   def project do
     [app: :pryin,
-     version: "0.3.0",
+     version: "1.0.0",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
